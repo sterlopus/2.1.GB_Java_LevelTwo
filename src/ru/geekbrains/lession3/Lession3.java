cdded3626edd3c10af5a1f9d60530228747359fb
Gleb Smirnov <sterlopus@gmail.com>
2021-08-19 22:18:52 +0300

    basic function added

---
+++ src/ru/geekbrains/lession3/Lession3.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.lession3;
+
+public class Lession3 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ru/geekbrains/lession3/Orange.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.lession3;
+
+public class Orange extends Fruit{
+}
