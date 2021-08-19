8b0f427938a1fb481e20b12bb2841b452a70efc4
Gleb Smirnov <sterlopus@gmail.com>
2021-08-19 22:18:52 +0300

    basic function added to Exceptions

---
+++ src/ru/geekbrains/lession3/Apple.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.lession3;
+
+public class Apple extends Fruit{
+}
---
+++ src/ru/geekbrains/lession3/Box.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.lession3;
+
+import java.util.ArrayList;
+
+public class Box <T extends Fruit> {
+    private T ArrayList
+
+    public Box() {
+    this.
+    }
+
+    ArrayList
+    public float getWeight (T ){
+
+    }
+
+}
